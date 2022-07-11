205dbc0bbea82270922e4cb89d199fd67755daed
parimalavasipalli10 <59381796+parimalavasipalli10@users.noreply.github.com>
2022-07-11 11:45:26 +0530

    Update A.java

--- A.java
+++ A.java
@@ -2,4 +2,7 @@ public class A{
 	public static void main(String[] abc){
 		System.out.println("Hello from main/master branch");
 	}
+	public void msg(){
+		System.out.println("CHANGE IS HAPPENED");
+	}
 }
